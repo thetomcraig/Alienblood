822eba62a1722026d484c9198850268a310ad366
Tom Craig <thetomcraig@icloud.com>
2018-09-15 21:29:35 -0700

    Remove vim files

--- vim/install_vim_files.sh
+++
@@ -1,9 +0,0 @@
-# Assuming a BREW install of vim
-vim_version=$(ls /usr/local/share/vim | grep 'vim[!0-9][!0-9]')
-cp cst.vim /usr/local/share/vim/${vim_version}/syntax/
-cp xenomorph.vim /usr/local/share/vim/${vim_version}/colors/
-mkdir ~/.vim/colors
-cp xenomorph.vim ~/.vim/colors
-
-# Asumming a VUNDLE install of airline
-cp xenomorph_airline.vim ${HOME}/.vim/bundle/vim-airline/autoload/airline/themes/xenomorph.vim
