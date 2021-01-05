5b0513e48d4f0e5b8b9183185406d4e1283a8c86
Tom Craig <thetomcraig@icloud.com>
2021-01-04 16:11:04 -0800

    update with screenshot table and colors

---
+++ colors.sh
@@ -0,0 +1,32 @@
+# Basic Colors
+foreground='768e88'
+background='121c13'
+bold='87f798'
+links='005bbb'
+selection='255031'
+# These two the same
+selected_text='81f7a2'
+cursor='81f7a2'
+cursor_text='111d13'
+
+# ANSI
+black='14311c'
+red='923c33'
+green='398d31'
+yellow='838f2e'
+blue='3b7c91'
+magenta='596c91'
+cyan='3d9089'
+white='778e88'
+
+bright_black='4c5815'
+bright_red='e79201'
+bright_green='00e000'
+bright_yellow='c8e200'
+bright_blue='00b8e5'
+bright_magenta='0071e6'
+bright_cyan='00e3ce'
+bright_white='81f7a2'
+
+# Vim Specific
+foreground_closer_to_background=
